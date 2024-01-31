9fc38169e747d9103c69e1caf0b1d6e2ff7aeac0
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-01-30 21:10:52 -0800

    add css and js file

---
+++ script.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+
+    // fadeIn elements starts
+    var logoIcon = $(".logo-icon")
+      logoIcon.delay(300).fadeIn(5000);
+      logoIcon.animate ({
+        opacity: 600
+      });
+        
+    var menuContainer = $(".menu-file-container")
+      menuContainer.delay(300).fadeIn(5000);
+      menuContainer.animate ({
+        opacity: 600
+      });
+    
+    var mathFavorites = $(".favorites")
+      mathFavorites.delay(300).fadeIn(5000);
+      mathFavorites.animate ({
+        opacity: 600
+      });
+        
+    var foodImg = $(".food img")
+      foodImg.delay(300).fadeIn(5000);
+      foodImg.animate ({
+        opacity: 600
+      });
+        
+    var location = $(".location")
+      location.delay(300).fadeIn(5000);
+      location.animate ({
+        opacity: 600
+      });
+    
+    var locationTitle = $(".location-title")
+      locationTitle.delay(300).fadeIn(5000);
+      locationTitle.animate ({
+        opacity: 600
+      });
+        
+    var locationP = $(".location p")
+      locationP.delay(300).fadeIn(5000);
+      locationP.animate ({
+        opacity: 600
+      });
+        
+    var hours = $(".hours")
+      hours.delay(300).fadeIn(5000);
+      hours.animate ({
+        opacity: 600
+      });
+    
+    //fadeIn elements ends    
+        });
+    
